e998c0d6083d569bb6c94e1a7345ae4f56acc4d0
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-05-21 22:33:48 +0500

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Hina Khan on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
