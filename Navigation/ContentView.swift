0233f13326d5e4d3b6493dd1fcbce6932f8daf70
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-06-05 23:14:22 +0500

    doing navigation inside a list of 1000 rows

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -12,8 +12,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            NavigationLink("Tap Me") {
-                DetailView(number: 556)
+            List(0..<1000) { i in
+                NavigationLink("Tap Me") {
+                    DetailView(number: i)
+                }
             }
         }
     }
