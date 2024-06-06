e083df8eef9828f09941029b7945189d6e4a702b
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-06-06 23:49:46 +0500

    navigation Destination

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -12,10 +12,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            List(0..<1000) { i in
-                NavigationLink("Tap Me") {
-                    DetailView(number: i)
-                }
+            List(0..<100) { i in
+                NavigationLink("Select \(i)", value: i)
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected \(selection)")
             }
         }
     }
