fab99a2df01c6da0887ae591a9dd32a13ec4e33c
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-06-09 22:35:12 +0500

    add buttons

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -16,7 +16,17 @@ struct ContentView: View {
     var body: some View {
             NavigationStack(path: $path) {
                 VStack {
-                    // more code to come
+                    Button("Show 32") {
+                        path = [32]
+                    }
+                    
+                    Button("Show 64") {
+                        path.append(64)
+                    }
+                    
+                    Button("Show 32 then 64") {
+                        path = [32, 64]
+                    }
                 }
                 .navigationDestination(for: Int.self) { selection in
                     Text("You selected \(selection)")
