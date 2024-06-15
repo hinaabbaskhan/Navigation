824502ffd5c141ef9f3122fba3677b264df24416
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-06-15 23:42:28 +0500

    save NavigationStack paths using Codable

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -6,6 +6,41 @@
 //
 
 import SwiftUI
+import Foundation
+
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            
+            
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+
+        // Still here? Start with an empty path.
+        path = NavigationPath()
+    }
+
+    func save() {
+        do {
+            let data = try JSONEncoder().encode(path)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
 
 
 
